3b7d0df588765c5185c03abc62de8ba0b775dc77
josephKwarteng2 <josephkwarteng244@gmail.com>
2024-12-09 16:22:39 +0000

    feat(auth): add DTOs for user registration and login with validation

---
+++ express-auth/src/dto/UserDTO.ts
@@ -0,0 +1,41 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsOptional,
+  IsEnum,
+} from "class-validator";
+import { Role } from "../models/types";
+import { FIELD_VALIDATION } from "../constants/constants";
+
+export class RegisterUserDTO {
+  @IsNotEmpty({ message: FIELD_VALIDATION.NAME_REQUIRED })
+  @IsString()
+  name!: string;
+
+  @IsEmail({}, { message: FIELD_VALIDATION.EMAIL_FORMAT })
+  @MaxLength(100, { message: FIELD_VALIDATION.EMAIL_MAX_LENGTH })
+  email!: string;
+
+  @IsNotEmpty()
+  @MinLength(6, { message: FIELD_VALIDATION.PASSWORD_LENGTH_ERROR })
+  password!: string;
+
+  @IsOptional()
+  @IsEnum(["Admin", "Manager", "User"], {
+    message: FIELD_VALIDATION.INVALID_ROLE,
+  })
+  role?: Role;
+}
+
+export class LoginUserDTO {
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  @MinLength(6, { message: FIELD_VALIDATION.PASSWORD_LENGTH_ERROR })
+  password!: string;
+}
