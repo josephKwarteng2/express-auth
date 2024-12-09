cdd5fa32ca9da59b1ba2909cdedbeea0e22d7623
josephKwarteng2 <josephkwarteng244@gmail.com>
2024-12-09 15:02:22 +0000

    chore(auth): add dev dependencies

--- express-auth/src/index.ts
+++ express-auth/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
+import bodyParser from "body-parser";
+import authRoute from "./router/AuthRoutes";
+import { globalErrorHandler } from "./utils/errorHandler";
 
 const app = express();
 
@@ -7,6 +10,9 @@ AppDataSource.initialize()
   .then(async () => {
     console.log("Data Source has been initialized!");
     app.listen(3000, () => {
+      app.use(bodyParser.json());
+      app.use("/api", authRoute);
+      app.use(globalErrorHandler);
       console.log("Express server has started on port 3000");
     });
   })
