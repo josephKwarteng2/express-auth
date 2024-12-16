0aa016c220a1012c7f683954e74429ad30660bdb
josephKwarteng2 <josephkwarteng244@gmail.com>
2024-12-16 22:31:16 +0000

    feat(auth): configure `CORS`

--- express-auth/src/index.ts
+++ express-auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { AppDataSource } from "./data-source";
 import bodyParser from "body-parser";
 import authRoute from "./router/AuthRoutes";
@@ -6,9 +7,24 @@ import { globalErrorHandler } from "./utils/errorHandler";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Origin",
+    ],
+    credentials: true,
+  })
+);
+
 AppDataSource.initialize()
   .then(async () => {
     console.log("Data Source has been initialized!");
+
     app.listen(3000, () => {
       app.use(bodyParser.json());
       app.use("/api", authRoute);
